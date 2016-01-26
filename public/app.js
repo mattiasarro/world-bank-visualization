dd6b5bb1f44a81974d063d765ef713f0c024f950
mattiasarro <mattias.arro@gmail.com>
2016-01-26 01:10:17 +0100

    refactor

--- public/app.js
+++ public/app.js
@@ -1,12 +1,14 @@
 vis = angular.module('vis', []);
 
 vis.controller('GraphController', function($scope, $http) {
+        
         $scope.limits = {
             startYear: 1994,
             endYear: 2014,
             startPercent: 0,
             endPercent: 100
         }
+        
         $scope.regions = {
             "ECS": {name: "Europe and Central Asia", state: "visible"},
             "NAC": {name: "North America", state: "visible"},
@@ -17,46 +19,60 @@ vis.controller('GraphController', function($scope, $http) {
             "SSF": {name: "Sub-Saharan Africa", state: "visible"},
         };
         
-        
-        $scope.countriesRegions = {};
+        $scope.blurb = "";
+        $scope.countries = {}; // {code: {code: "", name: "", region: "", dataPoints: []}}
         
         $http.get('country-regions.csv').success(function(text) {
+            var countriesRegions = {};
             var regions = d3.csv.parseRows(text);
             for (i = 1; i < regions.length; i++) {
-              $scope.countriesRegions[regions[i][0]] = regions[i][1];
+              countriesRegions[regions[i][0]] = regions[i][1];
             }
-        });
 
-
-        $scope.countryCodes = {};
-        $scope.data = {};
-
-        $http.get('internet-usage-countries.csv').success(function(text) {
-            var countries = d3.csv.parseRows(text);
-            for (i = 1; i < countries.length; i++) {
-                var values = countries[i].slice(4, countries[i.length - 1]);
-                $scope.countryCodes[countries[i][3]] = countries[i][2];
-                $scope.data[countries[i][3]] = values;
-            }
+            $http.get('internet-usage-countries.csv').success(function(text) {
+                var row = d3.csv.parseRows(text);
+                for (i = 1; i < row.length; i++) {
+                    var values = row[i].slice(4, row[i.length - 1]);
+                    var countryCode = row[i][3];
+                    
+                    dataPoints = [];
+                    for (j = 0; j < values.length; j++) {
+                        if (values[j] != '' && values[j] != '..') {
+                            dataPoints.push({
+                                yearIndex: j,
+                                perCent: values[j]
+                            });
+                        }
+                    }
+                    
+                    $scope.countries[countryCode] = {
+                        code: countryCode,
+                        name: row[i][2],
+                        region: countriesRegions[countryCode],
+                        current: false,
+                        dataPoints: dataPoints
+                    };
+                }
+            });
         });
 
         
         $scope.toggleRegion = function(id) {
             var countries = d3.selectAll("path." + id);
-            switch(graph.regions[id]["state"]) {
+            switch($scope.regions[id]["state"]) {
                 case "hidden":
                     countries.style("visibility", "visible");
-                    graph.regions[id]["state"] = "visible";
+                    $scope.regions[id]["state"] = "visible";
                 break;
                 
                 case "visible":
                     countries.classed('highlight', true);
-                    graph.regions[id]["state"] = "highlighted";
+                    $scope.regions[id]["state"] = "highlighted";
                 break;
                 case "highlighted":
                     countries.classed('highlight', false);
                     countries.style("visibility", "hidden");
-                    graph.regions[id]["state"] = "hidden";
+                    $scope.regions[id]["state"] = "hidden";
                 break;
             }
         }
@@ -72,9 +88,7 @@ vis.directive("graph", function() {
         var endYear = scope.$parent.limits.endYear;
         var startPercent = scope.$parent.limits.startPercent;
         var endPercent = scope.$parent.limits.endPercent;
-        
-        var countries_regions = scope.$parent.countriesRegions;
-        
+                
         var y = d3.scale.linear().domain([endPercent, startPercent]).range([0 + margin, height - margin]);
         var x = d3.scale.linear().domain([startYear, endYear]).range([0 + margin - 5, width]);
         var years = d3.range(startYear, endYear+1);
@@ -83,6 +97,8 @@ vis.directive("graph", function() {
                                        .append("svg:g");
         var line = d3.svg.line().x(function(d) { return x(d.x) })
                                 .y(function(d) { return y(d.y) });
+        var countryLine = d3.svg.line().x(function(d) { return x(years[d.yearIndex]) })
+                                       .y(function(d) { return y(d.perCent) });
         
         var xPoints = [[ {x: startYear, y: startPercent} , {x: endYear, y: startPercent} ]]; // not sure why array inside array necessary
         var yPoints = [[ {x: startYear, y: startPercent} , {x: startYear, y: endPercent} ]];
@@ -114,56 +130,46 @@ vis.directive("graph", function() {
                                 .attr("y2", y)
                                 .attr("x2", x(startYear));
 
-        scope.$watch('data', function(countryData) {
-            angular.forEach(countryData, function(data, countryCode) {
-                currData = [];
-                for (j = 0; j < data.length; j++) {
-                    if (data[j] != '' && data[j] != '..') {
-                        currData.push({
-                            x: years[j],
-                            y: data[j]
-                        });
-                    }
-                }
-                vis.append("svg:path")
-                .data([currData])
-                .attr("country", countryCode)
-                .attr("class", countries_regions[countryCode])
-                .attr("d", line)
-                .on("mouseover", onmouseover)
-                .on("mouseout", onmouseout);
+        scope.$watch('countries', function(countries) {
+            var data = [];
+            angular.forEach(countries, function(country, countryCode) {
+                data.push(country);
             });
+            
+            var countryLines = vis.selectAll("path.country-line").data(data, function(d) { return d.code; });
+            countryLines.enter().append("svg:path")
+                                .classed("current", function(d) { return d.current })
+                                .attr("class", function(d) { return(d.region) })
+                                .attr("country", function(d) { return(d.code) })
+                                .attr("d", function(d) { return countryLine(d.dataPoints) })
+                                .on("mouseover", activate)
+                                .on("mouseout", deactivate);
+            countryLines.exit().remove();
         }, true);
 
-        function onclick(d, i) {
-          var currClass = d3.select(this).attr("class");
-          if (d3.select(this).classed('selected')) {
-            d3.select(this).attr("class", currClass.substring(0, currClass.length - 9));
-          } else {
-            d3.select(this).classed('selected', true);
-          }
+        function activate(d, i) {
+            // console.log(d);
+            // d.current = true;
+            // console.log(d);
+            // d3.select(this).data(d);
+            scope.$apply(function(){
+                scope.blurb = d.name;
+            });
         }
 
-        function onmouseover(d, i) {
-          var currClass = d3.select(this).attr("class");
-          d3.select(this).attr("class", currClass + " current");
-          var countryCode = $(this).attr("country");
-          var blurb = '<h2>' + countryCodes[countryCode] + '</h2>';
-          $("#blurb-content").html(blurb);
-        }
-
-        function onmouseout(d, i) {
-          var currClass = d3.select(this).attr("class");
-          var prevClass = currClass.substring(0, currClass.length - 8);
-          d3.select(this).attr("class", prevClass);
-          $("#default-blurb").show();
-          $("#blurb-content").html('');
+        function deactivate(d, i) {
+            // d.current = false;
+            // d3.select(this).data(d);
+        //   d3.select(this).classed("current", false);
+          scope.$apply(function(){
+              scope.blurb = "";
+          });
         }
         
     }
     return {
         link: link,
         restrict: 'A',
-        scope: { data: '=' }
+        scope: { countries: '=', blurb: '=' }
     }
 });

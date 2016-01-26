b373ee52e68d54b04918bdd09a42c05bc93c4bd7
mattiasarro <mattias.arro@gmail.com>
2016-01-26 17:46:05 +0100

    add country name to right of graph, remove blurb, refactor

--- public/app.js
+++ public/app.js
@@ -1,6 +1,6 @@
-vis = angular.module('vis', []);
+app = angular.module('app', []);
 
-vis.controller('GraphController', function($scope, $http) {
+app.controller('GraphController', function($scope, $http) {
         
         $scope.limits = {
             startYear: 1994,
@@ -19,7 +19,6 @@ vis.controller('GraphController', function($scope, $http) {
             "SSF": {name: "Sub-Saharan Africa", state: "visible"},
         };
 
-        $scope.blurb = "";
         $scope.countries = {};
         
         $http.get('country-regions.csv').success(function(text) {
@@ -47,13 +46,13 @@ vis.controller('GraphController', function($scope, $http) {
                     
                     var alpha2 = iso_3366_1_Alpha3_to_Alpha2[countryCode];
                     
-                    if (alpha2 != undefined) {
+                    if (alpha2 != undefined && dataPoints.length > 0) {
                         $scope.countries[countryCode] = {
                             code: countryCode,
                             codeAlpha2: alpha2,
                             name: row[i][2],
                             regionCode: countriesRegions[countryCode], // ECS | NAN, etc
-                            current: false,
+                            active: false,
                             state: "visible", // highlighted | hidden
                             dataPoints: dataPoints
                         };
@@ -62,7 +61,25 @@ vis.controller('GraphController', function($scope, $http) {
             });
         });
 
-        $scope.toggleCountry = function(id) {}
+        $scope.toggleCountry = function(country) {
+            $scope.countries[country.code].active = !$scope.countries[country.code].active;
+            $scope.countries[country.code].activePersistent = !$scope.countries[country.code].activePersistent;
+        }
+        
+        
+        $scope.setCurrent = function (d) { 
+            if (d.activePersistent) { return; }
+            $scope.$apply(function(){ // needed since this func used by D3
+                $scope.countries[d.code].active = true;
+            });
+        }
+        
+        $scope.unsetCurrent = function(d) {
+            if (d.activePersistent) { return; }
+            $scope.$apply(function(){ // needed since this func used by D3
+                $scope.countries[d.code].active = false;
+            });
+        }
         
         $scope.toggleRegion = function(regionCode) {
             var mapping = {
@@ -72,30 +89,29 @@ vis.controller('GraphController', function($scope, $http) {
             }
             var currentState = $scope.regions[regionCode]["state"];
             var newState = mapping[currentState];
-            setState(regionCode, newState, $scope);
-        }
-        
-        function setState(regionCode, state, $scope) {
-            $scope.regions[regionCode]["state"] = state;
+            $scope.regions[regionCode]["state"] = newState;
+            
             angular.forEach($scope.countries, function(country, countryCode) {
                 if (country.regionCode == regionCode) {
-                    country.state = state;   
+                    country.state = newState;   
                 }
             });
         }
+        
 });
 
-vis.directive("graph", function() {
+app.directive("graph", function() {
     function link(scope, element, attr) {
         var width = 925;
         var height = 550;
         var margin = 30;
+        var rightMargin = 150;
         
-        var y,x,years,vis,line,countryLine;
+        var y,x,years,endYear, vis,line,countryLine;
         
         scope.$watch('limits', function(countries) {
             var startYear = scope.limits.startYear;
-            var endYear = scope.limits.endYear;
+                endYear = scope.limits.endYear;
             var startPercent = scope.limits.startPercent;
             var endPercent = scope.limits.endPercent;
                     
@@ -103,12 +119,12 @@ vis.directive("graph", function() {
             x = d3.scale.linear().domain([startYear, endYear]).range([0 + margin - 5, width]);
             years = d3.range(startYear, endYear+1);
             vis = d3.select(element[0]).append("svg:svg")
-                                           .attr("width", width).attr("height", height)
-                                           .append("svg:g");
+                                        .attr("width", width + rightMargin).attr("height", height)
+                                        .append("svg:g");
             line = d3.svg.line().x(function(d) { return x(d.x) })
-                                    .y(function(d) { return y(d.y) });
+                                .y(function(d) { return y(d.y) });
             countryLine = d3.svg.line().x(function(d) { return x(years[d.yearIndex]) })
-                                           .y(function(d) { return y(d.perCent) });
+                                       .y(function(d) { return y(d.perCent) });
             
             var xPoints = [[ {x: startYear, y: startPercent} , {x: endYear, y: startPercent} ]]; // not sure why array inside array necessary
             var yPoints = [[ {x: startYear, y: startPercent} , {x: startYear, y: endPercent} ]];
@@ -118,12 +134,12 @@ vis.directive("graph", function() {
             vis.selectAll(".xLabel").data(x.ticks(5))
                                     .enter().append("svg:text")
                                     .attr("class", "xLabel")
-                                    .text(String).attr("x", function(d) { return x(d) })
+                                    .text(String).attr("x", x)
                                     .attr("y", height - 10).attr("text-anchor", "middle");
             vis.selectAll(".yLabel").data(y.ticks(4))
                                     .enter().append("svg:text")
                                     .attr("class", "yLabel")
-                                    .text(String).attr("x", 0).attr("y", function(d) { return y(d) })
+                                    .text(String).attr("x", 0).attr("y", y)
                                     .attr("text-anchor", "right").attr("dy", 3);
             vis.selectAll(".xTicks").data(x.ticks(5))
                                     .enter().append("svg:line")
@@ -152,29 +168,27 @@ vis.directive("graph", function() {
             defineBehavior(countryLines.enter().append("svg:path")); // enter()
             countryLines.exit().remove();
             
+            defineCountryNameBehavior(vis.selectAll("text.countryName"));
+            defineCountryNameBehavior(countryLines.enter().append("svg:text"));
+            
+            function defineCountryNameBehavior(selection) {
+                selection
+                    .attr("class", function(d) { return(d.active ? "countryName countryNameActive" : "countryName") })
+                    .text(function(d) { return d.name })
+                    .attr("x", function(d) { return x(endYear + 0.2) })
+                    .attr("y", function(d) { return y(d.dataPoints[d.dataPoints.length-1].perCent) })
+                    .attr("text-anchor", "left").attr("dy", 3);
+            }
+            
             function defineBehavior(selection) { // since behavior is same for enter() and update(), pull it into a function
                 selection.attr("class", function(d) { return(d.regionCode + " country-line") })
-                         .classed("current", function(d) { return d.current })
+                         .classed("active", function(d) { return d.active })
                          .attr("country", function(d) { return(d.code) })
                          .attr("d", function(d) { return countryLine(d.dataPoints) })
                          .style("visibility", function(d) { return(d.state) }) // if "highlighted", just interpreted as visible
                          .classed('highlighted', function(d) { return(d.state == "highlighted" ? true : false) })
-                         .on("mouseover", activate)
-                         .on("mouseout", deactivate);
-            }
-            
-            function activate(d, i) {
-                scope.$apply(function(){
-                    scope.blurb = d.name;
-                    scope.countries[d.code].current = true;
-                });
-            }
-            
-            function deactivate(d, i) {
-                scope.$apply(function(){
-                    scope.blurb = "";
-                    scope.countries[d.code].current = false;
-                });
+                         .on("mouseover", scope.$parent.setCurrent)
+                         .on("mouseout", scope.$parent.unsetCurrent);
             }
         }, true);
         
@@ -182,6 +196,6 @@ vis.directive("graph", function() {
     return {
         link: link,
         restrict: 'A',
-        scope: { countries: '=', blurb: '=', limits: '=' }
+        scope: { countries: '=', limits: '=', setCurrent: '=', unsetCurrent: '=' }
     }
 });

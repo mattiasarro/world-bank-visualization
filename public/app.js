3ca747df780783c78bd23f97a42aa36352646598
mattiasarro <mattias.arro@gmail.com>
2016-01-27 17:56:08 +0100

    new choropleth with 2014 data

--- public/app.js
+++ public/app.js
@@ -93,7 +93,7 @@ app.directive("graph", function() {
         
         var y,x,years,endYear, vis,line,countryLine;
         
-        scope.$watch('limits', function(countries) {
+        scope.$watch('limits', function() {
             var startYear = scope.limits.startYear;
                 endYear = scope.limits.endYear;
             var startPercent = scope.limits.startPercent;
@@ -220,23 +220,129 @@ app.directive("graph", function() {
 
 app.directive("map", function() {
     function link(scope, element, attr) {
-        scope.$watch('year', function(year) {
+        var width, height, projection, path, graticule, svg, attributeArray = [], currentAttribute = 4, playing = false, countries;
+        setMap();
+        scope.$watch('countries', function(countries) {
+            loadData(countries); // let's load our data next
 
-            var map = d3.geomap.choropleth()
-            .geofile('vendor/countries.json')
-            .colors(colorbrewer.YlGnBu[9])
-            .column(year + ' [YR' + year + ']')
-            .format(function(d) { return d3.round(d) + '%' })
-            .legend(true)
-            .unitId('Country Code');
-            d3.select("#map svg").remove();
-            d3.csv('internet-usage-countries.csv', function(error, data) {
-                d3.select('#map')
-                .datum(data)
-                .call(map.draw, map);
+        }, true);
+        
+        
+        function setMap(countries) {
+            width = 960, height = 580; // map width and height, matches 
+            projection = d3.geo.eckert5() // define our projection with parameters
+                .scale(170)
+                .translate([width / 2, height / 2])
+                .precision(.1);
+
+            path = d3.geo.path() // create path generator function
+                .projection(projection); // add our define projection to it
+
+            graticule = d3.geo.graticule(); // create a graticule
+
+            svg = d3.select("#map").append("svg") // append a svg to our html div to hold our map
+                .attr("width", width)
+                .attr("height", height);
+
+            svg.append("defs").append("path") // prepare some svg for outer container of svg elements
+                .datum({
+                    type: "Sphere"
+                })
+                .attr("id", "sphere")
+                .attr("d", path);
+
+            svg.append("use") // use that svg to style with css
+                .attr("class", "stroke")
+                .attr("xlink:href", "#sphere");
+
+            svg.append("path") // use path generator to draw a graticule
+                .datum(graticule)
+                .attr("class", "graticule")
+                .attr("d", path);
+        }
+        
+        function loadData(countries) {
+            console.log(countries);
+            d3.json("vendor/world-topo.json", function(error, world) {
+                var countriesGeo = world.objects.countries.geometries; // store the path in variable for ease
+                for (var i in countriesGeo) { // for each geometry object
+                    var id = countriesGeo[i].properties.id;
+                    for (var k in countries[id]) {
+                        countriesGeo[i].properties[k] = countries[id][k];
+                    }
+                    // console.log(countriesGeo[i].properties);
+                }
+                drawMap(world); // let's mug the map now with our newly populated data object
             });
+        }
 
-        });
+        function drawMap(world) {
+
+            svg.selectAll(".country") // select country objects (which don't exist yet)
+                .data(topojson.feature(world, world.objects.countries).features) // bind data to these non-existent objects
+                .enter().append("path") // prepare data to be appended to paths
+                .attr("class", "country") // give them a class for styling and access later
+                .attr("id", function(d) {
+                    return "code_" + d.properties.id;
+                }, true) // give each a unique id for access later
+                .attr("d", path); // create them using the svg path generator defined above
+
+            d3.selectAll('.country') // select all the countries
+                .attr('data-percent', function(d) {
+                    var dataPoint = d.properties.dataPoints == null ? 0 : d.properties.dataPoints[10];
+                    return(dataPoint.perCent);
+                })
+                .style('fill', getColor);
+        }
+
+        function getColor(d) {
+            var perCent = d.properties.dataPoints == null ? 0 : d.properties.dataPoints[d.properties.dataPoints.length-1].perCent;
+            console.log(d.properties.id, "perCent", perCent);
+            
+            var quantize = d3.scale.quantize()
+                .domain([0, 100])
+                .range(["#c6d8ef", "#9ecae1", "#6baed6", "#4292c6", "#2171b5", "#085192", "#08306b"]);
+
+        
+            return quantize(perCent); // return that number to the caller
+        }
+        
+        function sequenceMap() {
+            
+            var dataRange = getDataRange(); // get the min/max values from the current year's range of data values
+            d3.selectAll('.country').transition() //select all the countries and prepare for a transition to new values
+            .duration(750) // give it a smooth time period for the transition
+            .attr('fill-opacity', function(d) {
+                return getColor(d.properties[attributeArray[currentAttribute]], dataRange); // the end color value
+            })
+            
+        }
+        
+        function animateMap() {
+
+            var timer; // create timer object
+            d3.select('#play')
+                .on('click', function() { // when user clicks the play button
+                    if (playing == false) { // if the map is currently playing
+                        timer = setInterval(function() { // set a JS interval
+                            if (currentAttribute < attributeArray.length - 1) {
+                                currentAttribute += 1; // increment the current attribute counter
+                            } else {
+                                currentAttribute = 0; // or reset it to zero
+                            }
+                            sequenceMap(); // update the representation of the map 
+                            d3.select('#clock').html(attributeArray[currentAttribute]); // update the clock
+                        }, 2000);
+
+                        d3.select(this).html('stop'); // change the button label to stop
+                        playing = true; // change the status of the animation
+                    } else { // else if is currently playing
+                        clearInterval(timer); // stop the animation by clearing the interval
+                        d3.select(this).html('play'); // change the button label to play
+                        playing = false; // change the status again
+                    }
+                });
+        }
     }
     return {
         link: link,
